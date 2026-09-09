7e4e44c4276ed9f8b7f15e0a08c447a3a942d9cb
Shankar <32197732+shankamal@users.noreply.github.com>
2026-09-09 23:09:42 +0530

    Use flat-file AssumeRole session settings

--- compliance/aws/session.py
+++ compliance/aws/session.py
@@ -1,4 +1,5 @@
 import boto3
+
 from .types import AccountTarget
 
 
@@ -6,16 +7,21 @@ def session_for(target: AccountTarget, base_session=None):
     base = base_session or boto3.Session()
     if not target.role_arn:
         return base
+
     sts = base.client("sts")
-    response = sts.assume_role(
-        RoleArn=target.role_arn,
-        RoleSessionName="CSAGEComplianceAudit",
-        DurationSeconds=3600,
-    )
-    c = response["Credentials"]
+    assume_args = {
+        "RoleArn": target.role_arn,
+        "RoleSessionName": target.role_session_name,
+        "DurationSeconds": target.duration_seconds,
+    }
+    if target.external_id:
+        assume_args["ExternalId"] = target.external_id
+
+    response = sts.assume_role(**assume_args)
+    credentials = response["Credentials"]
     return boto3.Session(
-        aws_access_key_id=c["AccessKeyId"],
-        aws_secret_access_key=c["SecretAccessKey"],
-        aws_session_token=c["SessionToken"],
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
         region_name=base.region_name,
     )
